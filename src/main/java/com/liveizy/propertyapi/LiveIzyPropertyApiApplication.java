947a18281ea917372ee952e68ba848baba03accc
richiecodes <oyelekerichard@yahoo.com>
2020-07-31 12:48:35 +0100

    first commit

---
+++ src/main/java/com/liveizy/propertyapi/LiveIzyPropertyApiApplication.java
@@ -0,0 +1,13 @@
+package com.liveizy.propertyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveIzyPropertyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveIzyPropertyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liveizy/propertyapi/LiveIzyPropertyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveizy.propertyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveIzyPropertyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
